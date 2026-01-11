3059992be8f76410e656a03fb9dd22f94c65e3c7
benpyles <benpyles118@gmail.com>
2026-01-10 21:12:12 -0500

    got a window up and the plane moving up and down on arrow keys

---
+++ main.py
@@ -0,0 +1,50 @@
+#importing pygame
+import pygame
+
+#Set up constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+WHITE = (255, 255, 255)
+SKY_BLUE = (135, 206, 235)
+
+#Setup pygame
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+running = True
+
+#Player Pos
+player_x = 75
+player_y = SCREEN_HEIGHT / 2
+
+#Load Assets and set character
+player = pygame.image.load("kenney_pixel-shmup/Ships/ship_0000.png")
+player_rect = player.get_rect()
+player = pygame.transform.rotate(player, 90)
+player_rect.topleft = (player_x, player_y)
+
+#Game loop
+while running:   
+    #Handles quiting
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #Fill screen to wipe the last frame
+    screen.fill(SKY_BLUE)
+
+    #Player movement
+    keypress = pygame.key.get_pressed()
+    if keypress[pygame.K_UP]:
+        player_y -= 3
+    if keypress[pygame.K_DOWN]:
+        player_y += 3
+
+    #Rendering
+    player_rect.topleft = (player_x, player_y)
+    screen.blit(player, player_rect)
+    pygame.display.flip()
+
+    clock.tick(60) #limit fps to 60
+
+pygame.quit()
