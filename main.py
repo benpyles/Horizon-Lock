b9e75d3c330a93a89abd8ff0899ab3b93ea9c3de
benpyles <benpyles118@gmail.com>
2026-01-29 11:56:24 -0500

    added scrolling bg in menu and made my own menu assets instead of public ones

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import math
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 800
 WHITE = (255, 255, 255)
-SKY_BLUE = (135, 206, 235)
+SKY_BLUE = (224, 251, 252)
 PLAYER_HEIGHT = 32
 PLAYER_WIDTH = 32
 scroll = 0
 FPS = 60
 game_state = "menu"
+menu_scroll = 0
 
+pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
@@ -24,20 +26,53 @@ grass_bg = pygame.image.load("./Assets/Tiles/Grass/BG/Grass_BG.png")
 dirt_bg = pygame.image.load("./Assets/Tiles/Dirt/BG/DirtBg.png")
 bg_width = 600
 bg_height = grass_bg.get_height()
-play_button = pygame.image.load("Assets/Ui/Menu/PlayButton.png")
-play_button_scaled = pygame.transform.scale(play_button, (250, 250/3))
-play_button_rect = play_button_scaled.get_rect()
-play_button_hover = pygame.transform.scale(play_button, (300, 300/3))
-play_button_hover_rect = play_button_hover.get_rect()
-play_button_rect.topleft = (SCREEN_WIDTH / 2 - play_button_rect.width / 2, SCREEN_HEIGHT / 4 - play_button_rect.height / 2)
-play_button_hover_rect.topleft = (SCREEN_WIDTH / 2 - play_button_hover_rect.width / 2, SCREEN_HEIGHT / 4 - play_button_hover_rect.height / 2)
-setting_button = pygame.image.load("Assets/Ui/Menu/SettingsButton.png")
-setting_button_scaled = pygame.transform.scale(setting_button, (250, 250/3))
-setting_button_rect = setting_button_scaled.get_rect()
-setting_button_hover = pygame.transform.scale(setting_button, (300, 300/3))
-setting_button_hover_rect = setting_button_hover.get_rect()
-setting_button_rect.topleft = (SCREEN_WIDTH / 2 - setting_button_rect / 2)
 
+#Ui loading
+button_gap = 20
+setting_button = pygame.image.load("Assets/Ui/Menu/SettingsButton.png")
+setting_button_scaled = pygame.transform.scale(setting_button, (250, 3750/67))
+setting_button_rect = setting_button_scaled.get_rect()
+setting_button_hover = pygame.transform.scale(pygame.image.load("Assets/Ui/Menu/SettingsButtonHover.png"), (250, 3750/67))
+setting_button_hover_rect = setting_button_hover.get_rect()
+setting_button_rect.topleft = (SCREEN_WIDTH / 2 - setting_button_rect.width / 2, SCREEN_HEIGHT / 2 - setting_button_rect.height)
+setting_button_hover_rect.topleft = (SCREEN_WIDTH / 2 - setting_button_hover_rect.width / 2, SCREEN_HEIGHT / 2 - setting_button_hover_rect.height)
+play_button = pygame.image.load("Assets/Ui/Menu/PlayButton.png")
+play_button_scaled = pygame.transform.scale(play_button, (250, 3750/67))
+play_button_rect = play_button_scaled.get_rect()
+play_button_hover = pygame.transform.scale(pygame.image.load("Assets/Ui/Menu/PlayButtonHover.png"), (250, 3750/67))
+play_button_hover_rect = play_button_hover.get_rect()
+play_button_rect.topleft = (SCREEN_WIDTH / 2 - play_button_rect.width / 2, setting_button_rect.y - button_gap - play_button_rect.height)
+play_button_hover_rect.topleft = (SCREEN_WIDTH / 2 - play_button_hover_rect.width / 2, setting_button_rect.y - button_gap - play_button_rect.height)
+achievements_button = pygame.image.load("Assets/Ui/Menu/AchievementsButton.png")
+achievements_button_scaled = pygame.transform.scale(achievements_button, (250, 3750/67))
+achievements_button_rect = setting_button_scaled.get_rect()
+achievements_button_hover = pygame.transform.scale(pygame.image.load("Assets/Ui/Menu/AchievementsButtonHover.png"), (250, 3750/67))
+achievements_button_hover_rect = setting_button_hover.get_rect()
+achievements_button_rect.topleft = (SCREEN_WIDTH / 2 - achievements_button_rect.width / 2, setting_button_rect.y + button_gap + achievements_button_rect.height)
+achievements_button_hover_rect.topleft = (SCREEN_WIDTH / 2 - achievements_button_hover_rect.width / 2, setting_button_rect.y + button_gap + achievements_button_rect.height)
+cloud = pygame.image.load("Assets/Ui/Menu/Cloud.png")
+cloud_rect = cloud.get_rect()
+menu_music = pygame.mixer.Sound("./Assets/Music/MenuMusic.wav")
+menu_music_playing = False
+menu_bg = pygame.image.load("Assets/Ui/Menu/Background.png").convert()
+menu_tiles = math.ceil((SCREEN_WIDTH / menu_bg.get_width())) + 1
+
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = cloud
+        self.rect = self.image.get_rect()
+        self.rect.x = SCREEN_WIDTH
+        self.rect.y = random.randint(0, SCREEN_HEIGHT - menu_bg.get_height())
+        self.speed = random.randint(2,6)
+    def update(self):
+        self.rect.x -= self.speed
+        if self.rect.right < 0:
+            self.kill()
+
+cloud_group = pygame.sprite.Group()
+
+#Player loading
 player = pygame.image.load("Assets/Ships/ship_0000.png")
 player_rect = player.get_rect()
 player_rect.topleft = (player_x, player_y)
@@ -51,7 +86,7 @@ grass_obstacles = [pygame.transform.scale(pygame.image.load(f"Assets/Tiles/Grass
 tiles = math.ceil((SCREEN_HEIGHT / bg_height)) + 1
 
 async def main():
-    global SCREEN_HEIGHT, play_button_rect, play_button, play_button_scaled, SCREEN_WIDTH, game_state,  WHITE, SKY_BLUE, scroll, PLAYER_HEIGHT, PLAYER_WIDTH, screen, clock, running, player_x, player_y, velocity, player, player_rect, biome, dirt_obstacles, grass_obstacles, loaded_obstacles, grass_bg, dirt_bg, bg_width, bg_height
+    global SCREEN_HEIGHT, menu_scroll, menu_bg, menu_music, menu_music_playing, cloud_group, play_button_rect, cloud, cloud_rect, play_button, play_button_scaled, SCREEN_WIDTH, game_state,  WHITE, SKY_BLUE, scroll, PLAYER_HEIGHT, PLAYER_WIDTH, screen, clock, running, player_x, player_y, velocity, player, player_rect, biome, dirt_obstacles, grass_obstacles, loaded_obstacles, grass_bg, dirt_bg, bg_width, bg_height
 
     last_obstacle_spawn_time = pygame.time.get_ticks()
 
@@ -59,17 +94,53 @@ async def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
         if game_state == "menu":
+            if not menu_music_playing:
+                menu_music.play(-1)
+                menu_music_playing = True
+            cloud_spawn = random.randint(1,60)
+            if cloud_spawn == 50:
+                new_cloud = Cloud()
+                cloud_group.add(new_cloud)
             screen.fill(SKY_BLUE)
+            cloud_group.update()
+            cloud_group.draw(screen)
+            for i in range(0, menu_tiles):
+                screen.blit(menu_bg, (i * menu_bg.get_width() + scroll, 500))
+            scroll -= 2
+
+            #reset scroll
+            if abs(scroll) > bg_width:
+                scroll = 0
+
             if play_button_rect.collidepoint(pygame.mouse.get_pos()):
                 screen.blit(play_button_hover, play_button_hover_rect)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    print("Clicked")
+                    game_state = "playing"
+
             else:
                 screen.blit(play_button_scaled, play_button_rect)
             if setting_button_rect.collidepoint(pygame.mouse.get_pos()):
                 screen.blit(setting_button_hover, setting_button_hover_rect)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    game_state = "settings"
             else:
                 screen.blit(setting_button_scaled, setting_button_rect)
+            if achievements_button_rect.collidepoint(pygame.mouse.get_pos()):
+                screen.blit(achievements_button_hover, achievements_button_hover_rect)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    game_state = "achievements"
+            else:
+                screen.blit(achievements_button_scaled, achievements_button_rect)
+            
+        if game_state == "settings":
+            print("settings")
+            
+
         if game_state == "playing":
+            menu_music_playing = False
             screen.fill(SKY_BLUE)
             current_time = pygame.time.get_ticks()
 
